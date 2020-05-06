fac99fdd39c9c72aa58b4593c0d93712b01e7ec8
Aaron D. Campbell <aaron@AaronDCampbell.com>
2020-05-05 21:16:40 -0500

    Add support GMT+-XX timezone formats

--- commands/rewards.js
+++ commands/rewards.js
@@ -20,6 +20,36 @@ module.exports = {
 				// Use moment-timezone for timezone handling
 				let moment = require('moment-timezone');
 
+				// Support GMT+-XX format for timezones
+				moment.tz.link([
+					'GMT-12|Etc/GMT+12',
+					'GMT-11|Etc/GMT+11',
+					'GMT-10|Etc/GMT+10',
+					'GMT-9|Etc/GMT+9',
+					'GMT-8|Etc/GMT+8',
+					'GMT-7|Etc/GMT+7',
+					'GMT-6|Etc/GMT+6',
+					'GMT-5|Etc/GMT+5',
+					'GMT-4|Etc/GMT+4',
+					'GMT-3|Etc/GMT+3',
+					'GMT-2|Etc/GMT+2',
+					'GMT-1|Etc/GMT+1',
+					'GMT-0|GMT',
+					'GMT+0|GMT',
+					'GMT+1|Etc/GMT-1',
+					'GMT+2|Etc/GMT-2',
+					'GMT+3|Etc/GMT-3',
+					'GMT+4|Etc/GMT-4',
+					'GMT+5|Etc/GMT-5',
+					'GMT+6|Etc/GMT-6',
+					'GMT+7|Etc/GMT-7',
+					'GMT+8|Etc/GMT-8',
+					'GMT+9|Etc/GMT-9',
+					'GMT+10|Etc/GMT-10',
+					'GMT+11|Etc/GMT-11',
+					'GMT+12|Etc/GMT-12'
+				]);
+
 				// Turns minute-based offsets to an offset string, ex 300 -> +0500 or -150 -> -0230
 				function tzOffsetMinutesToString( m ) {
 					return ((m < 0)?'-':'+') + Math.floor( Math.abs( m ) / 60 ).toString().padStart( 2, '0' ) + ( Math.abs( m ) % 60).toString().padStart( 2, '0' );
