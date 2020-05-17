80ceb5a92816959768e6062e3d8fbacaf5a667ad
Aaron D. Campbell <aaron@AaronDCampbell.com>
2020-05-17 11:23:20 -0500

    Add heroes command based on one from github.com/BrokenDesign/HWBot using data set from there too - props @BrokenDesign

---
+++ commands/heroes.js
@@ -0,0 +1,96 @@
+// require the discord.js module
+const Discord = require( 'discord.js' );
+const config = require( '../loadConfig.js' );
+
+const heroData = require( '../resources/hero-data.json' );
+
+module.exports = {
+	name: 'hero', // command name
+	description: 'Information about heroes', // Description
+	usage: 'list \\|\\| Hero', // Usage to offer to user
+	guildOnly: true, // If this needs to be in a server channel
+	aliases: ['heroes'],
+	execute( { message = {}, args = [], rawArgs = '', commandName = '' } ) {
+		// If the command used was the plural an no parameters were specified, assume they want a list of all heroes
+		if ( 'heroes' === commandName && !args.length ) {
+			args = [ 'list' ];
+		}
+
+		// If the request is for a "list" or for "all"
+		if ( [ 'list', 'all' ].includes( args[0].toLowerCase() ) ) {
+			return message.channel.send( Object.keys( heroData ).join( '\n' ) );
+		}
+
+		// No hero was specified
+		if ( ! args.length || ! args[0] ) {
+			return message.reply( `Please specify a hero or use \`${config.prefix}${this.name} list\` to see available heroes` );
+		}
+
+		const fixHeroName = function ( heroName ) {
+			return heroName.split(/[\s_-]/).map(s => s[0].toUpperCase() + s.substr(1).toLowerCase()).join( '_' );
+		}
+		let heroName, hero;
+
+		/**
+		 * As long as there are more args and we don't have a valid hero, combine first two args and try again
+		 * Handles two word names and leaves room for 3+ word names in the future
+		 */
+		while ( ( heroName = fixHeroName( args[0] ) ) && ! heroData[ heroName ] && args[1] ) {
+			// Combine first two args with space as second arg
+			args[1] = args[0] + ' ' + args[1];
+			// Shift off first arg
+			args.shift();
+		}
+
+		if ( heroData[ heroName ] && (hero = heroData[ heroName ]) && 'object' === typeof hero ) {
+			let formatSkills = function( skills ) {
+				let skillsStr = '';
+				for ( let [color, skill] of Object.entries( skills ) ) {
+					skillsStr += `**${color}:** ${skill.name}\n` +
+						`${skill.description}\n` +
+						`${skill.calculation}\n`;
+				}
+				return skillsStr;
+			}
+
+			// Embed to display
+			let heroEmbed = new Discord.MessageEmbed()
+				.setThumbnail( hero.image )
+				.setColor( '#9013FE' )
+				.setTitle( `**Hero Data: ${hero.name}**` )
+				.setDescription( hero.description )
+				.addField( '**Stone Source**', hero['stone source'], true )
+				.addField( '**Main Stat**', hero['main stat'], true )
+				.addField( '**Attack Type**', hero['attack type'], true )
+				.addField( '**Role**', hero['role'], true )
+				.addField( '**Skills**', formatSkills( hero.skills ) )
+				.addField( '**Glyphs**', '```' + Object.values( hero.glyphs ).join( '\n' ) + '```' )
+				.addField( '**Skins**', '```' + Object.values( hero.skins ).join( '\n' ) + '```' )
+				.addField( '**Artifacts**', '```' + Object.values( hero.artifacts ).join( '\n' ) + '```' );
+
+
+			/*
+					embed.set_thumbnail(url=hero['image'])
+
+					def format_skill(skill):
+					fstring = f"**{skill[0]}:** {skill[1]['name']}\n" \
+										  f"{skill[1]['description']}\n" \
+										  f"{skill[1]['calculation']}"
+					return fstring
+
+			 */
+			/*
+			const exampleEmbed = new Discord.MessageEmbed()
+		.setTitle('Some title')
+		.attachFiles(['../assets/discordjs.png'])
+		.setImage('attachment://discordjs.png');
+
+	channel.send(exampleEmbed);
+			 */
+
+			return message.channel.send( heroEmbed );
+		} else {
+			return message.reply( `No hero found, use \`${config.prefix}${this.name} list\` to see available heroes` );
+		}
+	},
+};
