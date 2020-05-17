4a0ed51e9317dcd212518dda4a1bd065838cbc0d
Aaron D. Campbell <aaron@AaronDCampbell.com>
2020-05-16 21:17:27 -0500

    Remove commands that were ust for testing, learning, and debugging

--- commands/user-info.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'user-info',
-	description: 'User Info!',
-	execute( message, args ) {
-		message.channel.send( `Your username: ${message.author.username}\nYour ID: ${message.author.id}` );
-	},
-};
