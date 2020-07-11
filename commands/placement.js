c531ad7058bcaa5b9ccef7fe9e31b00a6b1d6c01
Aaron D. Campbell <Aaron@AaronDCampbell.com>
2020-07-11 08:34:49 -0500

    Add Corvus and Morrigan to placement

--- commands/placement.js
+++ commands/placement.js
@@ -12,8 +12,8 @@ module.exports = {
 			.setColor( '#9013FE' )
 			.setTitle( '**HERO BATTLE PLACEMENT**' )
 			.setDescription( "This is the order of heros. Chabba will sit in front of Aurora who sits in front of Cleaver etc... this is nice to keep in mind when planning out your teams. This is especially useful if you want to use Nebula or Dorian who are very placement specific with their buffs." )
-			.addField( 'Front Line', "```\nChabba\nAurora\nCleaver\nLuther\nZiri\nRufus\nAstaroth\nGalahad\nIsh\nK'arkh\nMarkus\nElmir\nLilith\nAndvari\nQingMao\nSatori\n```", true )
-			.addField( 'Mid Line', "```\nMaya\nArachne\nDante\nKrista\nKiera\nJudge\nCeleste\nKai\nJhu\nSebastian*\nNebula\nMojo\nHedi\nJorgen\n```", true )
+			.addField( 'Front Line', "```\nChabba\nAurora\nCleaver\nLuther\nCorvus\nZiri\nRufus\nAstaroth\nGalahad\nIsh\nK'arkh\nMarkus\nElmir\nLilith\nAndvari\nQingMao\nSatori\n```", true )
+			.addField( 'Mid Line', "```\nMaya\nArachne\nDante\nKrista\nKiera\nJudge\nMorrigan\nCeleste\nKai\nJhu\nSebastian*\nNebula\nMojo\nHedi\nJorgen\n```", true )
 			.addField( 'Back Line', "```\nOrion\nGinger\nDareDevil\nDarkStar\nLars\nAstrid\nCornelius\nFaceless\nFox\nLian\nPhobos\nArtemis\nDorian\nPeppy\nJet\nThea\nHelios\nMartha\n```", true )
 		;
 
